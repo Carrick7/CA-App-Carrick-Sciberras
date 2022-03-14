39ea04df15067fb5c11b20d96b5ffbf5c92f7e5e
Carrick7 <92592671+Carrick7@users.noreply.github.com>
2022-03-14 22:58:00 +0000

    continuous delivery. Editing deploy.sh to fix issue

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@ sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js witha built-in load balancer
 sudo npm install -g pm2
 # stop any instance of our application running currently
-pm2 stop ExampleApplication
+pm2 stop CA-App-Carrick-Sciberras
 # change directory into folder where application is downloaded
-cd ExampleApplication/
+cd CA-App-Carrick-Sciberras/
 # Install application dependencies
 npm install
 # pm2 save error
 sudo pm2 save
 # Start the applciation with the process name ExampleApplication using pm2
-pm2 start ./bin/www --name ExampleApplication
+pm2 start ./bin/www --name CA-App-Carrick-Sciberras
